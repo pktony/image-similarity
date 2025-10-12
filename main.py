4cbbbfdd244ff598afe12aa655f5d9d56868b4a0
Psangwon <pktony@naver.com>
2025-10-12 23:20:41 +0900

    feat: similarity calculator (cosine similiarity)

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+    print("Hello from pokemon-similarity!")
+
+    # extract image embedding
+
+    # input image
+    # calculate similarity
+
+    
+
+
+if __name__ == "__main__":
+    main()
